23ebb6d168eea4beab09f3b8582d05178c182766
chknwaffles <wonsae.kim@gmail.com>
2019-07-14 03:06:54 -0400

    websockets way

--- src/components/Message.js
+++ src/components/Message.js
@@ -1,7 +1,6 @@
 import React from 'react'
 
 export default function Message(props) {
-    console.log(props)
     return (
         <React.Fragment>
             <p> {props.username}: {props.message} </p>
--- src/containers/MessageContainer.js
+++ src/containers/MessageContainer.js
@@ -1,24 +1,29 @@
 import React, { useState, useEffect } from 'react'
 import MessageForm from '../components/MessageForm'
 import Message from '../components/Message'
-import io from 'socket.io-client';
 
-const socket = io.connect('http://localhost:3000')
+const ws = new WebSocket('ws://localhost:3000')
 
 export default function MessageContainer() {
     const [log, setLog] = useState([])
 
     const handleMessageSubmit = (message) => {
-        socket.emit('message', { message })
+        ws.send(JSON.stringify(message))
         setLog([...log, message])
     }
 
     useEffect(() => {
-        socket.on('received', (obj) => {
-            console.log(obj)
-            setLog([...log, obj.data.message])
-        })
+        ws.onmessage = (e) => {
+            const message = JSON.parse(e.data)
+            setLog([...log, message])
+        }
     }, [log])
+
+    useEffect(() => {
+        ws.onopen = () => {
+            console.log('Connected!')
+        }
+    }, [])
 
     return (
         <div>
